6df52cf67d5dcb72fb04acf945f8c67c07712cb5
Caleb Owens <caleb@jason-freys-mac-mini.local>
2016-03-07 08:32:13 -0800

    Initial Commit

---
+++ JSON Courses/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JSON Courses
+//
+//  Created by Caleb Owens on 3/7/16.
+//  Copyright © 2016 Caleb Owens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
